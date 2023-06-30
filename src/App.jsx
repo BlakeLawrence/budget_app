60c80fb411e7304a965992fa22acd77b035a0685
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-06-30 08:33:23 +0100

    create browser router implemented on app.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+  },
+]);
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={Router} />
+    </div>
+  );
 }
 
 export default App;
